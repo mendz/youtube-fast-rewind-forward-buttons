dfdb9924d7d61b51eca832d176540df4f88dd818
Mendy Leizerovich <leizerovich.mendy@gmail.com>
2022-04-03 21:56:56 +0300

    chore(update-manifest): Upgrade to manifest version 3 (#7)

--- src/background.js
+++
@@ -1,5 +0,0 @@
-// because YouTube is a SPA, we need to check if there is a video in the page in every change in the URL
-chrome.webNavigation.onHistoryStateUpdated.addListener((e) => {
-  // send a message to content.js that the URL has changed
-  chrome.tabs.sendMessage(e.tabId, { message: 'urlChanged' });
-});
