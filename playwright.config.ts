524de0183d708e7750e6b6afd65017410abe3f0f
Mendy Leizerovich <leizerovich.mendy@gmail.com>
2022-08-12 15:46:35 +0300

    learning + add tasks

--- playwright.config.ts
+++ playwright.config.ts
@@ -39,7 +39,8 @@ const config: PlaywrightTestConfig = {
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    // trace: 'on-first-retry',
+    trace: 'on',
   },
 
   /* Configure projects for major browsers */
