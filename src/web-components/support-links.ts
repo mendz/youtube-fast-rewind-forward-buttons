ebbc09629a84e76ba295be27d5930febeaaad162
Mendy Leizerovich <leizerovich.mendy@gmail.com>
2024-07-01 00:13:34 +0300

    update the link to be directly to the reviews page

--- src/web-components/support-links.ts
+++ src/web-components/support-links.ts
@@ -55,7 +55,7 @@ class SupportLinks extends HTMLElement {
                     <a href="https://www.buymeacoffee.com/leizerovich.mendy" target="_blank"><img
                         src="https://cdn.buymeacoffee.com/buttons/v2/lato-yellow.png" alt="Buy Me A Coffee"
                         style="height: 60px !important; width: 217px !important" /></a>
-                    <a href="https://chromewebstore.google.com/detail/youtube-rewind-fast-forwa/bmdiaadnpgbbfepggiiajgadlhhcphgk"
+                    <a href="https://chromewebstore.google.com/detail/youtube-rewind-fast-forwa/bmdiaadnpgbbfepggiiajgadlhhcphgk/reviews"
                     target="_blank" id="rate-us">Love the Extension? Please leave a review on the Chrome Web Store!</a>
                 </div>
 `;
