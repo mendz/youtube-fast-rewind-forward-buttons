59ae660c1cf2b31b3ce9e079f45a9fd882c9de7a
Mendy Leizerovich <leizerovich.mendy@gmail.com>
2024-08-13 22:02:53 +0300

    update style for `updates-page`

---
+++ src/background/updates-page/script.ts
@@ -0,0 +1,67 @@
+interface IUpdates {
+  title: string;
+  description: string;
+}
+
+interface IVersionUpdates {
+  version: string;
+  date: string;
+  updates: IUpdates[];
+}
+
+const versionUpdates: IVersionUpdates[] = [
+  {
+    version: '1.3.1',
+    date: getDate('2024-08-13'),
+    updates: [
+      {
+        title: 'New way to open the options page',
+        description:
+          'Now you can click on the extension icon in the top right corner. This will open a popup with a button that will open the options page.',
+      },
+      {
+        title: 'Support Buttons Added',
+        description: `We've added two new buttons in the options page and the new popup to make it easier for you to support the
+        development of this extension.`,
+      },
+    ],
+  },
+];
+
+setUpdates();
+
+function getDate(dateStr: string) {
+  const date = new Date(dateStr);
+  return new Intl.DateTimeFormat('en-IL', {
+    month: 'long',
+    day: '2-digit',
+    year: 'numeric',
+  }).format(date);
+}
+
+function setUpdates() {
+  const updatesElement = document.getElementById('updates') as HTMLDivElement;
+  const html = versionUpdates
+    .reverse()
+    .map(
+      (versionUpdate) => /* html */ `
+        <h3 class="version-date">${versionUpdate.version} - ${
+          versionUpdate.date
+        }</h3>
+        ${versionUpdate.updates
+          .map((update) => renderUpdate(update))
+          .join('\n')}
+    `
+    )
+    .join('\n');
+  updatesElement.innerHTML = html;
+}
+
+function renderUpdate(update: IUpdates): string {
+  return /* html */ `
+    <div class="card">
+      <h3>${update.title}</h3>
+      <p>${update.description}</p>
+    </div>
+    `;
+}
--- src/web-components/support-links.ts
+++ src/web-components/support-links.ts
@@ -1,63 +1,21 @@
 class SupportLinks extends HTMLElement {
-  connectedCallback() {
+  async connectedCallback() {
     const shadow = this.attachShadow({ mode: 'closed' });
 
+    const cssURL = chrome.runtime.getURL('web-components/support-links.css');
+    const responseCSS = await fetch(cssURL);
+    const cssStyles = await responseCSS.text();
+
     shadow.innerHTML = /* html */ `
-         <style>
-            div {
-                flex-direction: column;
-                align-items: center;
-                gap: 5px;
-                padding: 10px;
-                display: flex;
-                text-align: center;
+        <style>${cssStyles}</style>
 
-                & a {
-                    cursor: pointer;
-                }
-
-                & a#rate-us {
-                    color: #18272f;
-                    position: relative;
-                    text-decoration: none;
-                    display: inline-block;
-                    max-width: 300px;
-
-                    background-color: #afe2ed;
-                    border-radius: 5px;
-                    box-shadow: 4px 3px 6px 0px rgba(0, 0, 0, 0.57);
-                    padding: 0.5rem;
-
-                    &::before {
-                    content: '';
-                    position: absolute;
-                    width: 100%;
-                    height: 4px;
-                    border-radius: 4px;
-                    background-color: #18272f;
-                    bottom: -4px;
-                    left: 0;
-                    transform-origin: right;
-                    transform: scaleX(0);
-                    transition: transform 0.3s ease-in-out;
-                    }
-
-                    &:hover::before {
-                    transform-origin: left;
-                    transform: scaleX(1);
-                    }
-                }
-            }
-
-                </style>
-
-                <div>
-                    <a href="https://www.buymeacoffee.com/leizerovich.mendy" target="_blank"><img
-                        src="https://cdn.buymeacoffee.com/buttons/v2/lato-yellow.png" alt="Buy Me A Coffee"
-                        style="height: 60px !important; width: 217px !important" /></a>
-                    <a href="https://chromewebstore.google.com/detail/youtube-rewind-fast-forwa/bmdiaadnpgbbfepggiiajgadlhhcphgk/reviews"
-                    target="_blank" id="rate-us">Love the Extension? Please leave a review on the Chrome Web Store!</a>
-                </div>
+        <div>
+            <a href="https://www.buymeacoffee.com/leizerovich.mendy" target="_blank"><img
+                    src="https://cdn.buymeacoffee.com/buttons/v2/lato-yellow.png" alt="Buy Me A Coffee"
+                    style="height: 60px !important; width: 217px !important" /></a>
+            <a href="https://chromewebstore.google.com/detail/youtube-rewind-fast-forwa/bmdiaadnpgbbfepggiiajgadlhhcphgk/reviews"
+            target="_blank" id="rate-us">Love the Extension? Please leave a review on the Chrome Web Store!</a>
+        </div>
 `;
   }
 }
