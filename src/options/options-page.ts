88b9a90a9fe7f0085ff3556a0039e4c38ef02ae1
Mendy Leizerovich <leizerovich.mendy@gmail.com>
2025-04-21 02:22:31 +0300

    feat: Update Pico CSS import and enhance layout styling for options page

--- src/options/options-page.ts
+++ src/options/options-page.ts
@@ -1,5 +1,5 @@
-import '@picocss/pico/css/pico.classless.min.css';
 import '@picocss/pico/css/pico.colors.min.css';
+import '@picocss/pico/css/pico.min.css';
 import { numberFormat } from '../content/helper';
 import { IOptions, IStorageOptions, Prettify } from '../content/types';
 
